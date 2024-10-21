3d5bd0d21b17888dae4a5cd5f76de0bfdd838f71
epltv1 <159470517+epltv1@users.noreply.github.com>
2024-10-22 00:10:16 +0300

    Update Matches.tsx

--- src/sections/Matches.tsx
+++ src/sections/Matches.tsx
@@ -131,3 +131,26 @@ const Matches = () => {
 };
 
 export default Matches;
+
+{
+    "id": string,
+    "title": string,
+    "category": string,
+    "date": number, // unix timestamp in milliseconds
+    "poster": string | undefined,
+    "popular": boolean,
+    "teams": undefined | {
+        "home": undefined | {
+            "name": string,
+            "badge": string,
+        },
+        "away": undefined | {
+            "name": string,
+            "badge": string,
+        },
+    },
+    "sources": {
+        "source": string,
+        "id": string,
+    }[]
+      }
